97cdfa4d93496ea7a385d07d2eed312ad332043f
raghavop <raghavopnhihai@gmail.com>
2024-01-30 14:28:23 -0500

    this time computer guess our number

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,4 +9,23 @@ def guess(x):
         elif guess > random_number:
             print('sorry, guess again , Too large.')
     print(f'Yay, congrats. You guess the number {random_number}')
-guess(10)
+
+def computer_guess(x):
+    low = 1 
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low     
+        feedback = input(f'Is {guess} Too high (H), Too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1 
+    print(f'You guessed {guess}')
+computer_guess(10)
+
+
+
